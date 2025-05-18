fe0a2d4a4b3cd86c76182356de77612c5630884a
Kirk Mendoza <kirkmendoza@gmail.com>
2025-05-18 18:49:35 -0500

    Added user check on Home page.

--- src/components/Auth/Home.jsx
+++ src/components/Auth/Home.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom"
 
-const Home = () => {
+const Home = ({ user }) => {
     return (
         <div className="homeContainer">
             <h2>Welcome to Book Buddy - The Library App.</h2>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy5UKJvRRjvrOLiP-txPQ7fJjLNQppoTqV3hov6K7e01qEuMXEyVOC60Cs8in9EgomZjA&usqp=CAU" />
             <h3>Come browse our library of books!</h3>
-            <h4> See one you're interested in?<br/><Link to="/books" className="registerNowLink">Log in</Link> to reserve books!<br/>Not a member? <Link to="/register" className="registerNowLink">Register now</Link>!</h4>
+            {
+                user.id ? (
+                    <>
+                    <h3> Glad you're back, {user.firstname}!</h3><h4>Don't forget to check & update your,<br/>book <Link to="/reservations" className="registerNowLink">reservations</Link> or view your <Link to={`/account/${user.id}`} className="registerNowLink">account</Link>!</h4>
+                    </>
+                ) : (
+                    <h4> See one you're interested in?<br/><Link to="/books" className="registerNowLink">Log in</Link> to reserve books!<br/>Not a member? <Link to="/register" className="registerNowLink">Register now</Link>!</h4>
+                )
+            }
         </div>
     )
 }
